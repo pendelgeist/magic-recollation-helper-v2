8bfb56390433b5fa5076bae1fde439a1f75d5c88
Dylan Hall <dhall@campuslabs.com>
2019-09-10 20:47:18 -0400

    even better file parsing and such

--- netcoreapp/Repositories/AllSetFiles/set-slimmer.jq.sh
+++ netcoreapp/Repositories/AllSetFiles/set-slimmer.jq.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-for file in BFZ.json; do
+# extract `AllSetFiles.zip` and then run this to map to reasonably sized JSON files for each set
+for file in *.json; do
   cat $file | jq '. | { booster: .boosterV3, name: .name, code: .code, totalSetSize: .totalSetSize, cards: [.cards[] | { name: .name, uuid: .uuid, cmc: .convertedManaCost, rarity: .rarity, color: {colors: .colors, colorIdentity: .colorIdentity}, type: {types: .types, subtypes: .subtypes, supertypes: .supertypes}}]}' > $file.slim
-  #cat $file | jq '.cards | { cards: { name: .name, uuid: .uuid, cmc: .convertedManaCost, rarity: .rarity, color: {colors: .colors, colorIdentity: .colorIdentity}, type: {types: .types, subtypes: .subtypes, supertypes: .supertypes}
-
-  #ghead -n -1 $file.slim.1 > $file.slim;
-  #echo "," >> $file.slim;
-  #tail -n +2 $file.slim.2 >> $file.slim;
-  #rm $file.slim.1 $file.slim.2;
+  filename=$(basename $file.slim .json.slim)  
+  mv $file.slim $filename.slim.json
 done
 
