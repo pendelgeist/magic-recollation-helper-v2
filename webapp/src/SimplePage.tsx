24432a81321a6395417cfef8b4f5ed41bf80c02b
Dylan Hall <dhall@campuslabs.com>
2019-09-10 21:10:57 -0400

    make simple page make call to `magic-set` * call dying because the json parse isn't working right

--- webapp/src/SimplePage.tsx
+++ webapp/src/SimplePage.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import api from './api';
 
 export interface SimplePageProps {
-  name: string
+  name: string;
 }
 
-const SimplePage = (props: SimplePageProps) => <div>Simple Page - {props.name}</div>;
+const SimplePage = (props: SimplePageProps) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    api
+      .get(`/magic-set`)
+      .then((response: any) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <div>Simple Page - {props.name}</div>;
+};
 
 export default SimplePage;
